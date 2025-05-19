9e294798f188c04fc3adca055dbe66bcc37ee04b
xiaobaobao <932824098@qq.com>
2025-05-19 09:39:56 +0800

    屏蔽【清理所有遗留事件】

--- server/src/fight/normalEvent/_0_base/_2_PlayerRoundStart.ts
+++ server/src/fight/normalEvent/_0_base/_2_PlayerRoundStart.ts
@@ -33,7 +33,7 @@ export class _2_PlayerRoundStart implements Event {
 
     doEvent(room: Room): void {
         //清理所有遗留事件
-        room.broadcast(ROUTER.roomEvent.CLEAR_FLY_CARD);
+        // room.broadcast(ROUTER.roomEvent.CLEAR_FLY_CARD);
         (EventManager.getEvent(room, _0_GameStartEvent.name) as _0_GameStartEvent).roundEvent.clear();
         room.playerAddNewHandCard(this.currentPlayer, GAME_CONFIG.ROUND_INIT_CARD_NUM, "回合开始");
         room.eventStack.push(new _3_PlayerRounding(this.currentPlayer));
